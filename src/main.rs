fbd768022ff2fb87acac3c7d2502066898807d73
kusabana <flower@arcadian.garden>
2025-02-11 22:09:02 -0500

    feature: initial work on commandline part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use bsp::Bsp;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to BSP file
+    bsp_path: PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let contents = std::fs::read(args.bsp_path)?;
+    let bsp = Bsp::parse(&contents).expect("failed to parse BSP file");
+
+    dbg!(bsp);
+
+    Ok(())
 }
