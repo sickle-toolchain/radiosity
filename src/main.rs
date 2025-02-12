82a0c3df22ac35d23fc270475a313607794dcb9f
kusabana <flower@arcadian.garden>
2025-02-11 22:59:17 -0500

    feature: get hdr-variants of lumps if requested

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::path::PathBuf;
 
 use bsp::Bsp;
 use clap::Parser;
+use lump_definitions::source::{ColorRGBExp32, Face, LumpDefinition};
 
 #[derive(Parser, Debug)]
 struct Args {
     /// Path to BSP file
     bsp_path: PathBuf,
+
+    /// Use high dynamic range lumps
+    #[arg(long)]
+    hdr: bool,
 }
 
 fn main() -> std::io::Result<()> {
@@ -15,7 +20,35 @@ fn main() -> std::io::Result<()> {
     let contents = std::fs::read(args.bsp_path)?;
     let bsp = Bsp::parse(&contents).expect("failed to parse BSP file");
 
-    dbg!(bsp);
+    let faces = bsp
+        .lump_data::<[Face], _>(if args.hdr {
+            LumpDefinition::FacesHdr
+        } else {
+            LumpDefinition::Faces
+        })
+        .expect("failed to parse [Face]");
+
+    dbg!(faces.len());
+
+    let lighting = bsp
+        .lump_data::<[ColorRGBExp32], _>(if args.hdr {
+            LumpDefinition::LightingHdr
+        } else {
+            LumpDefinition::Lighting
+        })
+        .expect("failed to parse [ColorRGBExp32]");
+
+    dbg!(lighting.len());
+
+    let world_lights = bsp
+        .lump_data::<[ColorRGBExp32], _>(if args.hdr {
+            LumpDefinition::WorldLightsHdr
+        } else {
+            LumpDefinition::WorldLights
+        })
+        .expect("failed to parse [WorldLight]");
+
+    dbg!(world_lights.len());
 
     Ok(())
 }
