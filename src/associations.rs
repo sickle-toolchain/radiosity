14d0a01a6d8e8126de2dd212dbab6bcd07d1e5dc
kusabana <flower@arcadian.garden>
2025-03-27 17:26:32 -0400

    fix: update incorrect association error messages

--- src/associations.rs
+++ src/associations.rs
@@ -12,7 +12,7 @@ impl Associated<TextureInfo> for Face {
     fn associated<'a>(&self, bsp: &'a Bsp<'a>) -> Ref<'a, TextureInfo> {
         let texture_info = bsp
             .lump_cast::<[TextureInfo], _>(LumpDefinition::TextureInfo)
-            .expect("Failed to get LumpDefinition::Edges");
+            .expect("Failed to get LumpDefinition::TextureInfo");
 
         assert!((self.texture_info_index as usize) < texture_info.len());
         Ref::map(texture_info, |texture_info| &texture_info[self.texture_info_index as usize])
@@ -23,7 +23,7 @@ impl Associated<Plane> for Face {
     fn associated<'a>(&self, bsp: &'a Bsp<'a>) -> Ref<'a, Plane> {
         let planes = bsp
             .lump_cast::<[Plane], _>(LumpDefinition::Planes)
-            .expect("Failed to get LumpDefinition::Edges");
+            .expect("Failed to get LumpDefinition::Planes");
 
         assert!((self.plane_index as usize) < planes.len());
         Ref::map(planes, |planes| &planes[self.plane_index as usize])
