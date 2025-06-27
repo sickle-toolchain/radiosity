558c6646403c0b593ec0444a9717afae5c5ade3c
kusabana <flower@arcadian.garden>
2025-06-27 10:36:02 -0400

    Perform device creation and teardown

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::path::Path;
+use std::error::Error;
+
+use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let shader_crate_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("radiosity-shader");
+    println!("cargo::rerun-if-changed={}", shader_crate_path.join("Cargo.toml").display());
+    println!("cargo::rerun-if-changed={}", shader_crate_path.join("src").join("lib.rs").display());
+
+    SpirvBuilder::new(shader_crate_path, "spirv-unknown-vulkan1.2")
+        .extension("SPV_KHR_ray_tracing")
+        .capability(Capability::RayTracingKHR)
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
+
+    Ok(())
+}
