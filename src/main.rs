b2126de48ac7a4bfb2e677c801d707be0730eb13
kusabana <flower@arcadian.garden>
2025-07-26 10:24:55 -0400

    Build top-level acceleration structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![feature(gen_blocks)]
 use std::{collections::HashSet, error::Error, ffi::CStr, os::raw::c_char, path::PathBuf};
 
-use ash::{prelude::VkResult, util::Align, vk};
+use ash::{
+    prelude::VkResult,
+    util::Align,
+    vk::{self, Packed24_8},
+};
 use bsp::Bsp;
 use clap::Parser;
 use lump_definitions::source::{Edge, Face, LumpDefinition, SurfaceEdge, Vertex};
@@ -197,8 +201,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         .iter()
         .flat_map(|face| {
             let surface_edges = <Face as Associated<[SurfaceEdge]>>::associated(face, &bsp);
-            let indices = surface_edges.into_iter().map(|surface_edge| {
-                <SurfaceEdge as Associated<Edge>>::associated(&surface_edge, &bsp).edge
+            let indices = surface_edges.iter().map(|surface_edge| {
+                <SurfaceEdge as Associated<Edge>>::associated(surface_edge, &bsp).edge
                     [(surface_edge.edge_index < 0) as usize]
             });
 
@@ -275,10 +279,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let (blas, blas_buffer) = {
         let build_range_info = vk::AccelerationStructureBuildRangeInfoKHR::default()
-            .first_vertex(0)
-            .primitive_count(indices.len() as u32 / 3)
-            .primitive_offset(0)
-            .transform_offset(0);
+            .primitive_count(indices.len() as u32 / 3);
 
         let geometries = [geometry];
 
@@ -293,7 +294,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             acceleration_structure.get_acceleration_structure_build_sizes(
                 vk::AccelerationStructureBuildTypeKHR::DEVICE,
                 &build_info,
-                &[indices.len() as u32 / 3],
+                &[build_range_info.primitive_count],
                 &mut sizes_info,
             )
         };
@@ -373,18 +374,178 @@ fn main() -> Result<(), Box<dyn Error>> {
         (blas, blas_buffer)
     };
 
+    let accel_handle = {
+        let as_addr_info =
+            vk::AccelerationStructureDeviceAddressInfoKHR::default().acceleration_structure(blas);
+        unsafe { acceleration_structure.get_acceleration_structure_device_address(&as_addr_info) }
+    };
+
+    let instance_buffer = {
+        let instances = [vk::AccelerationStructureInstanceKHR {
+            transform: vk::TransformMatrixKHR {
+                matrix: [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            },
+            instance_custom_index_and_mask: Packed24_8::new(0, 0xff),
+            instance_shader_binding_table_record_offset_and_flags: Packed24_8::new(
+                0,
+                vk::GeometryInstanceFlagsKHR::TRIANGLE_FACING_CULL_DISABLE.as_raw() as u8,
+            ),
+            acceleration_structure_reference: vk::AccelerationStructureReferenceKHR {
+                device_handle: accel_handle,
+            },
+        }];
+        let instance_buffer_size =
+            std::mem::size_of::<vk::AccelerationStructureInstanceKHR>() * instances.len();
+
+        let mut instance_buffer = BufferResource::new(
+            instance_buffer_size as vk::DeviceSize,
+            vk::BufferUsageFlags::SHADER_DEVICE_ADDRESS
+                | vk::BufferUsageFlags::ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_KHR,
+            vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
+            &device,
+            device_memory_properties,
+        )?;
+
+        instance_buffer.store(&instances, &device);
+
+        instance_buffer
+    };
+
+    let (tlas, tlas_buffer) = {
+        let build_range_info =
+            vk::AccelerationStructureBuildRangeInfoKHR::default().primitive_count(1);
+
+        let build_command_buffer = {
+            let allocate_info = vk::CommandBufferAllocateInfo::default()
+                .command_buffer_count(1)
+                .command_pool(command_pool)
+                .level(vk::CommandBufferLevel::PRIMARY);
+
+            let command_buffers =
+                unsafe { device.allocate_command_buffers(&allocate_info) }.unwrap();
+            command_buffers[0]
+        };
+
+        unsafe {
+            device
+                .begin_command_buffer(
+                    build_command_buffer,
+                    &vk::CommandBufferBeginInfo::default()
+                        .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT),
+                )
+                .unwrap();
+            let memory_barrier = vk::MemoryBarrier::default()
+                .src_access_mask(vk::AccessFlags::TRANSFER_WRITE)
+                .dst_access_mask(vk::AccessFlags::ACCELERATION_STRUCTURE_WRITE_KHR);
+            device.cmd_pipeline_barrier(
+                build_command_buffer,
+                vk::PipelineStageFlags::TRANSFER,
+                vk::PipelineStageFlags::ACCELERATION_STRUCTURE_BUILD_KHR,
+                vk::DependencyFlags::empty(),
+                &[memory_barrier],
+                &[],
+                &[],
+            );
+        }
+
+        let instances = vk::AccelerationStructureGeometryInstancesDataKHR::default()
+            .array_of_pointers(false)
+            .data(vk::DeviceOrHostAddressConstKHR {
+                device_address: instance_buffer.device_address(&device),
+            });
+
+        let geometry = vk::AccelerationStructureGeometryKHR::default()
+            .geometry_type(vk::GeometryTypeKHR::INSTANCES)
+            .geometry(vk::AccelerationStructureGeometryDataKHR { instances });
+
+        let geometries = [geometry];
+
+        let mut build_info = vk::AccelerationStructureBuildGeometryInfoKHR::default()
+            .flags(vk::BuildAccelerationStructureFlagsKHR::PREFER_FAST_TRACE)
+            .geometries(&geometries)
+            .mode(vk::BuildAccelerationStructureModeKHR::BUILD)
+            .ty(vk::AccelerationStructureTypeKHR::TOP_LEVEL);
+
+        let mut sizes_info = vk::AccelerationStructureBuildSizesInfoKHR::default();
+        unsafe {
+            acceleration_structure.get_acceleration_structure_build_sizes(
+                vk::AccelerationStructureBuildTypeKHR::DEVICE,
+                &build_info,
+                &[build_range_info.primitive_count],
+                &mut sizes_info,
+            )
+        };
+
+        let tlas_buffer = BufferResource::new(
+            sizes_info.acceleration_structure_size,
+            vk::BufferUsageFlags::ACCELERATION_STRUCTURE_STORAGE_KHR
+                | vk::BufferUsageFlags::SHADER_DEVICE_ADDRESS
+                | vk::BufferUsageFlags::STORAGE_BUFFER,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL,
+            &device,
+            device_memory_properties,
+        )?;
+
+        let as_create_info = vk::AccelerationStructureCreateInfoKHR::default()
+            .ty(build_info.ty)
+            .size(sizes_info.acceleration_structure_size)
+            .buffer(tlas_buffer.buffer)
+            .offset(0);
+
+        let tlas =
+            unsafe { acceleration_structure.create_acceleration_structure(&as_create_info, None) }
+                .expect("Failed to create top-level acceleration structure");
+
+        build_info.dst_acceleration_structure = tlas;
+
+        let scratch_buffer = BufferResource::new(
+            sizes_info.build_scratch_size,
+            vk::BufferUsageFlags::SHADER_DEVICE_ADDRESS | vk::BufferUsageFlags::STORAGE_BUFFER,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL,
+            &device,
+            device_memory_properties,
+        )?;
+
+        build_info.scratch_data = vk::DeviceOrHostAddressKHR {
+            device_address: scratch_buffer.device_address(&device),
+        };
+
+        unsafe {
+            acceleration_structure.cmd_build_acceleration_structures(
+                build_command_buffer,
+                &[build_info],
+                &[&[build_range_info]],
+            );
+            device.end_command_buffer(build_command_buffer).unwrap();
+            device
+                .queue_submit(
+                    graphics_queue,
+                    &[vk::SubmitInfo::default().command_buffers(&[build_command_buffer])],
+                    vk::Fence::null(),
+                )
+                .expect("queue submit failed.");
+
+            device.queue_wait_idle(graphics_queue).unwrap();
+            device.free_command_buffers(command_pool, &[build_command_buffer]);
+            scratch_buffer.destroy(&device);
+        }
+
+        (tlas, tlas_buffer)
+    };
+
     unsafe {
         device.destroy_command_pool(command_pool, None);
     }
 
     unsafe {
         acceleration_structure.destroy_acceleration_structure(blas, None);
-        // acceleration_structure.destroy_acceleration_structure(top_as, None);
+        acceleration_structure.destroy_acceleration_structure(tlas, None);
     }
 
     blas_buffer.destroy(&device);
-    // top_as_buffer.destroy(&device);
+    tlas_buffer.destroy(&device);
 
+    instance_buffer.destroy(&device);
     vertex_buffer.destroy(&device);
     index_buffer.destroy(&device);
 
