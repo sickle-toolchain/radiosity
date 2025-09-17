9bd1f281a12fea7a58965c78f6699380cb5f936e
flower <flower@arcadian.garden>
2025-09-17 18:32:48 +0200

    Mark function dereferencing ptr arg as unsafe

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ struct Args {
     hdr: bool,
 }
 
-pub extern "system" fn vulkan_debug_utils_callback(
+pub unsafe extern "system" fn vulkan_debug_utils_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
     _message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
